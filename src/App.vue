bba02ec4d1161583035a6f22bdd1b94faa61accf
Humam Samra <Humamdaraya@gmail.com>
2023-11-22 19:49:50 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-navigation-drawer :width="251" v-model="drawer">
+      <v-list-item title="My Application" subtitle="Vuetify" value="title" class="my-3"></v-list-item>
+      <v-divider></v-divider>
+      <v-list nav>
+        <v-list-item prepend-icon="mdi-home-outline" subtitle="Navigate to homepage" title="Home"
+          value="home"></v-list-item>
+        <v-list-item prepend-icon="mdi-format-list-bulleted" subtitle="Navigate to finished tasks" title="Current Tasks"
+          value="list"></v-list-item>
+        <v-list-item prepend-icon="mdi-format-list-text" subtitle="Navigate to finished tasks" title="Finished"
+          value="finished"></v-list-item>
+        <v-list-item prepend-icon="mdi-calendar-check" subtitle="Navigate to finished tasks" title="Finished"
+          value="incomplete"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar elevation="2" color="accent">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>ToDo List</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+
+
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn icon="mdi-dots-vertical" v-bind="props"></v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="(item, index) in items" :key="index" :value="index">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <template v-slot:extension>
+        <v-container centered>
+          <v-tabs fixed-tabs>
+            <v-tab>
+              Todo List
+            </v-tab>
+            <v-tab>
+              Finished
+            </v-tab>
+            <v-tab>
+              Incomplete
+            </v-tab>
+          </v-tabs>
+        </v-container>
+      </template>
+    </v-toolbar>
+
+    <v-main>
+      <HomePage />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HomePage
+  },
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: 'Create' },
+      { title: 'Settings' },
+      { title: 'Theme' },
+    ],
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-container>
+        <v-text-field v-model="searchInp" clearable label="Search" variant="solo-filled"></v-text-field>
+        <h3>Tasks :</h3>
+        <v-btn stacked>
+            Button
+        </v-btn>
+    </v-container>
+    <v-card>
+        <v-list class="container">
+            <v-list-item class="p-3 rounded-2 mb-3" v-for="item in items" :value="item.value" :key="item.value">
+                <v-list-item-content>
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    <v-list-item-subtitle>{{ item.subtitle }}</v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+    </v-card>
+</template>
+<script>
+export default {
+    name: "HomePage",
+    data: () => ({
+        searchInp: "",
+        items: Array.from({ length: 10 }, (_, index) => ({
+            title: `Title ${index + 1}`,
+            subtitle: `Sub title ${index + 1} here`,
+            value: index
+        })),
+    }),
+    watch: {
+        searchInp(newText) {
+            this.search(newText);
+        },
+    },
+    methods: {
+        search(value) {
+            alert(value);
+        },
+    },
+}
+</script>
